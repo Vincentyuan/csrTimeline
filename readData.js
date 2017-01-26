8963553cbe84b618600629edab152ab90ea8192e
vincent <526730329@qq.com>
2017-01-26 12:49:00 +0100

    prepare for the lib and the html file read data

---
+++ readData.js
@@ -0,0 +1,19 @@
+//functions should be here
+
+function getRequiredData(yearStart, yearEnd, catalog,searchText){
+//get data according to user choose.
+//check all the parameter if parameter is null then ignore , if not null then apply the filter.
+//loop all the entry in the list 
+}
+
+function isValidEntry(entity,yearStart,yearEnd,catalog,searchText){
+//check the entry is valid or not if yes then return true;
+
+}
+//some functions for the filter
+function getCatalogs(){
+
+}
+function objCopy(obj){
+	return $.extend({}, obj);
+}
