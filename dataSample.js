59515e4fcf9a0f76e01ea55fd29c8a33c06e0594
vincent <526730329@qq.com>
2017-01-26 12:26:38 +0100

    data sample

---
+++ dataSample.js
@@ -0,0 +1,24 @@
+// this is the sample of data 
+
+// 属性可以包括并不限于这些
+var csrDataList = [{
+	id: 1,
+	year:1991,
+	catalog:"Social",
+	author:"xinyu",
+	title:"efrei become the best school in the world",
+	article:"",
+	articalLink:"",
+	imagePath:"",
+	discription:""
+},{
+	id: ,
+	year:1991,
+	catalog:"Social",
+	author:"xinyu",
+	title:"efrei become the best school in the world",
+	article:"",
+	articalLink:"",
+	imagePath:"",
+	discription:""
+}];
